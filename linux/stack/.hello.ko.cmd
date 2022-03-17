4b649daa1cf48e90e55ea02abd79c68d2cd81134
magnate3 <liangeaglejun@126.com>
2022-03-17 22:43:40 +0800

    del linux dir

--- linux/stack/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/SimplestLKM/hello.ko := ld -EL -r -maarch64linux -T ./scripts/module-common.lds --build-id  -o /root/SimplestLKM/hello.ko /root/SimplestLKM/hello.o /root/SimplestLKM/hello.mod.o ;  true
